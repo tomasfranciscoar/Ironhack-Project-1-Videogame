d80368e2a1fbe9bad4e08547163c2d70324d6f5a
Tomas Freire Knight <tomasfreire@gmail.com>
2019-02-24 20:44:51 -0600

    added instructions detail, score, time, game nav-bar

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var onePlayer = document.getElementById("onePlayerGame");
 var mainPannel = document.getElementsByClassName("main-pannel")[0];
 var mainGame = document.getElementsByClassName("main-game")[0];
 var menuButton = document.getElementById("menu-btn");
+var startGameButton = document.getElementById("start-game-btn");
+var instructionsButton = document.getElementById("instructions");
+var instructionsDetail = document.getElementsByClassName("instructions-detail")[0];
 
 onePlayer.onclick = function(){
   mainPannel.setAttribute("class", "main-pannel-off");
@@ -9,6 +12,20 @@ onePlayer.onclick = function(){
 }
 
 menuButton.onclick = function(){
+  // score = 0;
+  clearInterval(interval);
   mainGame.setAttribute("class", "main-game-off");
   mainPannel.setAttribute("class", "main-pannel main-pannel-on")
+}
+
+startGameButton.onclick = function(){
+  startGame();
+}
+
+instructionsButton.onmouseover = function(){
+  instructionsDetail.setAttribute("class", "instructions-detail")
+}
+
+instructionsButton.onmouseout = function(){
+  instructionsDetail.setAttribute("class", "instructions-detail instructions-detail-off")
 }
--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-frames = 0;
+var frames = 0;
+var interval;
+var score = 0;
+var time = 0;
 
 class Background{
   constructor(){
-    this.x = 0;
+    this.x = -80;
     this.y = 0;
     this.width = canvas.width*2;
     this.height = canvas.height;
     this.image = new Image();
     this.image.src = "./images/Fondo doble.png";
   }
+  time(){
+    if(frames % 60 === 0) time++
+    ctx.font = "15px pixelart";
+    ctx.fillStyle = "white";
+    ctx.fillText("TIME: " + time, 330, 50)
+  }
+  score(){
+    if(frames % 60 === 0) score++;
+    ctx.font = "15px pixelart";
+    ctx.fillStyle = "white";
+    ctx.fillText("SCORE: " + score, 60, 50)
+  }
+  infoNavBar(){
+    ctx.fillStyle = "gray";
+    ctx.globalAlpha = 0.5;
+    ctx.fillRect(25,18,750,50);
+    ctx.globalAlpha = 1.0;
+  }
   draw(){
-    if(this.x < -canvas.width*2) this.x = 0;
+    // ctx.fillStyle = grey;
+    // ctx.fillRect(0,0,canvas.width,canvas.height);
+    this.x--
+    if(this.x < -canvas.width*2) this.x = -80;
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     ctx.drawImage(this.image, this.x + this.width - 130, this.y, this.width, this.height);
   }
@@ -21,14 +45,35 @@ class Background{
 
 class Camion{
   constructor(){
-    
+    this.x = 100;
+    this.y = 310;
+    this.width = 130;
+    this.height = 70;
+    this.image = new Image();
+    this.image.src = "./images/Camion 1 right.png"
+  }
+  draw(){
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
 
 var background = new Background();
+var camion = new Camion();
 
-setInterval(function(){
+function update(){
   frames++;
   ctx.clearRect(0,0,canvas.width,canvas.height);
-  background.draw()
-}, 1000/60)
+  background.draw();
+  background.infoNavBar();
+  background.score();
+  background.time();
+  camion.draw();
+}
+
+function restart(){
+  
+}
+
+function startGame(){
+  interval = setInterval(update, 1000/60)
+}
