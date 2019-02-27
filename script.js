15d4893540b2871b4c129ede63e3fe19fa0e897b
Tomas Freire Knight <tomasfreire@gmail.com>
2019-02-26 21:48:21 -0600

    changed background movement and limits

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 var frames = 0;
 var interval;
-var gravity = 2;
+// var gravity = 2;
 var score = 0;
 var time = 0;
 var chavoTamalArr = [];
@@ -46,7 +46,8 @@ class Background{
   draw(){
     // ctx.fillStyle = grey;
     // ctx.fillRect(0,0,canvas.width,canvas.height);
-    this.x--
+    // this.x--
+    if(this.x > 0) this.x = 0;
     if(this.x < -canvas.width*2) this.x = -80;
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     ctx.drawImage(this.image, this.x + this.width - 130, this.y, this.width, this.height);
@@ -63,16 +64,28 @@ class Camion{
     this.image.src = "./images/Camion 1 right.png"
   }
   moveForward(){
-    this.x += 3
+    this.x += 4
   }
   moveBackwards(){
-    this.x -= 3
+    this.x -= 4
   }
-  jump(){
-    this.y -= 60
+  moveUp(){
+    this.y -= 4
   }
+  moveDown(){
+    this.y += 4
+  }
+  // jump(){
+  //   this.y -= 60
+  // }
   draw(){
-    if(this.y < canvas.height - 140) this.y += gravity;
+    if(this.x < 0) this.x = 0;
+    if(this.x > canvas.width-this.width-30){
+      this.x = canvas.width-this.width-30;
+      background.x -= 3
+    };
+    if(this.y < 220) this.y = 220;
+    if(this.y > 370) this.y = 370;
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
@@ -131,15 +144,9 @@ function startGame(){
 }
 
 addEventListener("keydown", function(e){
-  if(e.keyCode === 32){
-    camion.jump();
-  }
-  if(e.keyCode === 39){
-    camion.moveForward();
-    camion.image.src = "./images/Camion 1 right.png"
-  }
-  if(e.keyCode === 37){
-    camion.moveBackwards();
-    camion.image.src = "./images/Camion 1 left.png"
-  }
+  // if(e.keyCode === 32){camion.jump()}
+  if(e.keyCode === 39){camion.moveForward(); camion.image.src = "./images/Camion 1 right.png"; background.x--}
+  if(e.keyCode === 37){camion.moveBackwards(); camion.image.src = "./images/Camion 1 left.png"; background.x++}
+  if(e.keyCode === 38){camion.moveUp()}
+  if(e.keyCode === 40){camion.moveDown()}
 })
